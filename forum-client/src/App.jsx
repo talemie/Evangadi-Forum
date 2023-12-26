13094137335a8d81b9caf374bf874a0005a00cbc
temu man <talemie@yahoo.com>
2023-12-26 08:34:41 -0600

    dotenv file added + front end modified

--- forum-client/src/App.jsx
+++ forum-client/src/App.jsx
@@ -3,13 +3,15 @@ import './App.css'
 import Header from './Components/Header/Header'
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from './Components/Home/Home';
+import Footer from './Components/Footer/Footer';
 
 function App() {
 
   return (
     <>
       <Header />
-      <Home/>
+      <Home />
+      <Footer/>
     </>
   )
 }
