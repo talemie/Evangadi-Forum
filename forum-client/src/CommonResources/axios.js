a5b56201f81e053461df87628888db26658eb558
temu man <talemie@yahoo.com>
2024-01-09 08:35:58 -0600

    removed axios from server side

--- forum-client/src/CommonResources/axios.js
+++ forum-client/src/CommonResources/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-	baseURL: "http://localhost:5500/api",
+	baseURL: "http://localhost:8080/api",
+	// baseURL: "https://evangadi-forum-backend-tem.onrender.com/api",
 });
 export default instance;
